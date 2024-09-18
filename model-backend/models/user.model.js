01988d875cfa5f40778febfbad5afd06ca61e5b8
mitchelle lindsey <mitchellelindsey@gmail.com>
2024-09-18 16:23:49 +0000

    Set up user schema in user.model.js. Defined schema for user data, including fields for firstName, lastName, username, email, and password. Added data validation to ensure proper input types and formats. Implemented schema methods for password hashing using bcrypt and integration with JWT for authentication.

---
+++ model-backend/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, 
+{
+    timestamps: true,
+})
+
+const User = mongoose.model('users', UserSchema)
+
+module.exports = User
