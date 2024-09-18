c7621192c5a5d713922d445627780e240aa169fc
mitchelle lindsey <mitchellelindsey@gmail.com>
2024-09-18 16:30:36 +0000

    Enhanced the authentication features in index.js by implementing login and registration endpoints. This update allows users to create new accounts and log in with their existing credentials, incorporating password hashing for secure storage and comparison.

---
+++ model-backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./routes/auth.route')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World'))
+app.use('/api/auth', authRouter)
+
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@modelhub.2s7fk.mongodb.net/model-hub?retryWrites=true&w=majority&appName=modelhub`).then(() => {
+    console.log('Database is connected')
+    app.listen(process.env.PORT, () => console.log('Port Available'))
+})
