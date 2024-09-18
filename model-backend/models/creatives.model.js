867fa4dbeafe309c48312175bafceb074df4b352
mitchelle lindsey <mitchellelindsey@gmail.com>
2024-09-18 19:52:32 +0100

    database schema for creatives when signing up

---
+++ model-backend/models/creatives.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const Model = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    profileOverview: {
+        type: String,
+        required: true
+    },
+    stats: [{
+        name: String,
+        value: Number
+    }],
+    contact: [
+        {
+            name: String,
+            value: String
+        }
+    ],
+    socials: [
+        {
+            name: String,
+            value: String
+        }
+    ],
+    portfolio: {
+        type: String,
+        required: true
+    },
+    workExp: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
