a3d1c9d1ea426c50522fe700e6d024a889226397
mitchelle lindsey <mitchellelindsey@gmail.com>
2024-09-20 15:23:25 +0100

    feat: cors enabked

--- model-backend/index.js
+++ model-backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const authRouter = require('./routes/auth.route')
 require('dotenv').config()
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.use('/api/auth', authRouter)
